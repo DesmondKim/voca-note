3a474079e38b3cd46dfbe5f4d3009f6fa3739c48
Taesung Jung <tsj.july@gmail.com>
2014-08-29 22:56:08 +0900

    implemented upload feature

--- madrid.py
+++ madrid.py
@@ -28,6 +28,39 @@ class MainHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello')
 
+class VocabularyUploadHandler(webapp2.RequestHandler):
+
+    def get(self):
+        page = JINJA_ENVIRONMENT.get_template('pages/upload.html')
+        self.response.out.write(page.render())
+
+    def post(self):
+
+        note_name = self.request.get('note_name')
+        raw_voca_list = self.request.get('voca_list')
+
+        note_list = VocabularyNote.query(VocabularyNote.name==note_name).fetch()
+
+        # create Vocabulary Note if VocabularyNote named note_name
+        if 0 >= len(note_list):
+            voca_note = VocabularyNote()
+            voca_note.name = note_name
+            voca_note.put()
+
+        # insert vocabulary
+        for word in raw_voca_list.readlines():
+
+            # assumed each word separated by *
+            eng_kor_str = word.split('*')
+
+            voca = Vocabulary(parent=ndb.Key(VocabularyNote, note_name))
+            voca.eng = eng_kor_str[0]
+            voca.kor = eng_kor_str[1]
+            voca.again = False
+            voca.put()
+
 application = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/upload', VocabularyUploadHandler),
 ], debug=True)
+
